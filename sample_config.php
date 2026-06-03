4592cee24b4f4f08c5c487febea931660d6c251a
neumann <otarshenge@gmail.com>
2026-06-03 15:26:37 +0200

    sample config

---
+++ sample_config.php
@@ -0,0 +1,9 @@
+<?php
+// Only keep constants here
+if (!defined('DB_HOST')) {
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', '');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_CHARSET', 'utf8mb4');
+}
